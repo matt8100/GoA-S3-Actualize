7c6e33fa6004a47ef19b3aa04ae8294956e14316
Hod Kimhi <hodkimhi@gmail.com>
2020-03-22 17:42:26 -0700

    Removed extra folder 'TBDApp' and moved files up a level. Project root folder is now s03-rmd-mcmath1. Added XML layout files for activity_main, activity_messaging, activity_advisor_profile, fragment_explore, fragment_contacts, fragment_profile.

--- app/src/main/java/com/example/tbdapp/activities/MainActivity.java
+++ app/src/main/java/com/example/tbdapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tbdapp;
+package com.example.tbdapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tbdapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
