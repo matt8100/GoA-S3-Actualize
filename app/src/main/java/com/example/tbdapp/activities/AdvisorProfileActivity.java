dcd667631ec0160edba79f662c52a21dc9b536e8
Matthew Mao <matty8100@gmail.com>
2020-03-29 19:00:41 -0700

    created AdvisorProfileActivity.java and added the activity to AndroidManifest.xml

---
+++ app/src/main/java/com/example/tbdapp/activities/AdvisorProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.tbdapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tbdapp.R;
+
+public class AdvisorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advisor_profile);
+    }
+}
