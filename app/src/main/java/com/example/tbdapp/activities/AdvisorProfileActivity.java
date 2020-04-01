013b912dc4e170c6c10e5da703135657f1f827a4
Matthew Mao <matty8100@gmail.com>
2020-03-31 17:06:17 -0700

    testing functionality of receiving serializable extras

--- app/src/main/java/com/example/tbdapp/activities/AdvisorProfileActivity.java
+++ app/src/main/java/com/example/tbdapp/activities/AdvisorProfileActivity.java
@@ -1,10 +1,15 @@
 package com.example.tbdapp.activities;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.tbdapp.R;
+import com.example.tbdapp.activities.MainActivity.Advisor;
 
 public class AdvisorProfileActivity extends AppCompatActivity {
 
@@ -12,5 +17,35 @@ public class AdvisorProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advisor_profile);
+
+        // declare variables
+        final TextView advisorName = findViewById(R.id.advisorProfile_advisorName);
+        final TextView advisorType = findViewById(R.id.advisorProfile_advisorType);
+        final ImageView advisorImage = findViewById(R.id.advisorProfile_advisorImage);
+        final TextView advisorDescription = findViewById(R.id.advisorProfile_advisorDescription);
+        final TextView advisorSkills = findViewById(R.id.advisorProfile_advisorSkills);
+        final ImageButton backButton = findViewById(R.id.advisorProfile_backButton);
+
+        // back button
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        // receive advisor data
+        Advisor advisor = (Advisor) getIntent().getExtras().getSerializable("advisor");
+
+        // set text
+        advisorName.setText(advisor.getAdvisorName());
+        advisorType.setText(advisor.getAdvisorType());
+        advisorImage.setImageResource(advisor.getAdvisorImage());
+        advisorDescription.setText(advisor.getAdvisorDescription());
+        advisorSkills.setText(advisor.getAdvisorSkills());
+
     }
+
+    // Private properties
+
 }
--- app/src/main/java/com/example/tbdapp/activities/MainActivity.java
+++ app/src/main/java/com/example/tbdapp/activities/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.tbdapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tbdapp.R;
 import com.example.tbdapp.fragments.ContactsFragment;
@@ -21,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Create the advisors
         createAdvisorProfiles();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AdvisorProfileActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("advisor", advisorList.get(2));
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frameLayout, new ContactsFragment(advisorList));
