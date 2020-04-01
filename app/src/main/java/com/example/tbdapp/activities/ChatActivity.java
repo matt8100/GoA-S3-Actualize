be4c2602d308462adffce7e3967cbf3d15529b1b
Matthew Mao <matty8100@gmail.com>
2020-03-31 18:22:43 -0700

    created chat activity

---
+++ app/src/main/java/com/example/tbdapp/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.tbdapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tbdapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+        }
+}
