63e46842ecf7b2051e95115fccb14355f79675fd
Markus Nimi <markusnimi@gmail.com>
2020-04-21 22:43:01 -0700

    Replaced all instances of the redundant ContactItem view with the Advisor model, deleted ContactItem

--- app/src/main/java/com/example/tbdapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/tbdapp/activities/ChatActivity.java
@@ -1,91 +1,71 @@
 package com.example.tbdapp.activities;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.ImageView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.tbdapp.R;
-import com.example.tbdapp.models.Advisor;
+import com.example.tbdapp.models.Author;
 import com.example.tbdapp.models.Message;
-import com.example.tbdapp.models.Singleton;
-import com.stfalcon.chatkit.commons.ImageLoader;
-import com.stfalcon.chatkit.messages.MessageHolders;
+import com.example.tbdapp.models.Advisor;
 import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
 
     private MessagesList mMessagesList;
     private MessageInput mMessageInput;
-    private MessagesListAdapter<Message> mAdapter;
+    private Author mReceiver;
+    private Author mSender;
     private Date mDate;
-    private String mContactId;
-    private Toolbar toolbar;
+    private MessagesListAdapter<Message> mAdapter;
+    private ArrayList<Message> mMessageArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
 
-        mContactId = getIntent().getStringExtra("contactId");
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_arrow);
 
-        Advisor currentAdvisor = Singleton.getInstance().advisors.get(Integer.parseInt(mContactId)-1);
-        getSupportActionBar().setTitle(currentAdvisor.name);
-        getSupportActionBar().setSubtitle(currentAdvisor.advisorType.toString());
-
+        Advisor contact = (Advisor) getIntent().getSerializableExtra("contact");
         mMessagesList = findViewById(R.id.messagesList);
         mMessageInput = findViewById(R.id.input);
+        mReceiver = new Author("0", "Matthew", null);
+        mSender = new Author("1", contact.advisorName, null);
+        mDate = new Date();
+        mAdapter = new MessagesListAdapter<Message>(mSender.getId(), null);
+        mMessageArrayList = new ArrayList<>();
 
-        MessageHolders messageHolders = new MessageHolders();
-        messageHolders.setIncomingTextLayout(R.layout.item_custom_incoming_text_message);
-        messageHolders.setOutcomingTextLayout(R.layout.item_custom_outcoming_text_message);
-
-        mAdapter = new MessagesListAdapter<>("0", messageHolders, new ImageLoader() {
-            @Override
-            public void loadImage(ImageView imageView, String url, Object payload) {
-                int image = getApplicationContext().getResources().getIdentifier(url, "drawable", getApplicationContext().getPackageName());
-                imageView.setImageResource(image);
-            }
-        });
-
+        setTitle(mSender.getName());
         mMessagesList.setAdapter(mAdapter);
 
+        createMessages();
         loadMessages();
 
         mMessageInput.setInputListener(new MessageInput.InputListener() {
             @Override
             public boolean onSubmit(CharSequence input) {
                 //validate and send message
-                mDate = new Date();
-                Message message = new Message("0", input.toString(), Singleton.getInstance().user, mDate);
-                mAdapter.addToStart(message, true);
-                Singleton.getInstance().chatHistory.get(mContactId).add(0, message);
+                mAdapter.addToStart(new Message("0", input.toString(), mSender, mDate), true);
                 return true;
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_chat, menu);
-        return true;
+    private void createMessages() {
+        mMessageArrayList.add(new Message("0","Hi, how are you doing?", mSender, mDate));
+        mMessageArrayList.add(new Message("1","I'm doing great, how are you?", mReceiver, mDate));
+        mMessageArrayList.add(new Message("0","I'm doing fine as well.", mSender, mDate));
     }
 
     private void loadMessages() {
-        mAdapter.addToEnd(Singleton.getInstance().chatHistory.get(mContactId), false);
+        mAdapter.addToEnd(mMessageArrayList, true);
     }
 }
 
