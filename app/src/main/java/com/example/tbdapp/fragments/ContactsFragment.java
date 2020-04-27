3261d9d45a059808565ae4c34454a4549dee44a2
Matthew Mao <matty8100@gmail.com>
2020-04-26 18:02:59 -0700

    recreated with ChatKit library, successfully tested

--- app/src/main/java/com/example/tbdapp/fragments/ContactsFragment.java
+++ app/src/main/java/com/example/tbdapp/fragments/ContactsFragment.java
@@ -1,21 +1,26 @@
 package com.example.tbdapp.fragments;
 
-import android.view.LayoutInflater;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tbdapp.R;
 import com.example.tbdapp.models.Advisor;
-import com.example.tbdapp.views.adapters.ContactAdapter;
+import com.example.tbdapp.models.Author;
+import com.example.tbdapp.models.Dialog;
+import com.example.tbdapp.models.Singleton;
+import com.example.tbdapp.models.User;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.dialogs.DialogsList;
+import com.stfalcon.chatkit.dialogs.DialogsListAdapter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class ContactsFragment extends Fragment {
     ArrayList<Advisor> advisorList;
@@ -24,86 +29,23 @@ public class ContactsFragment extends Fragment {
         advisorList = advisors;
     }
 
-    final FragmentActivity thisActivity = getActivity();
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private LinearLayoutManager mLinearLayoutManager;
+    private DialogsList mDialogsListView;
+    private DialogsListAdapter mAdapter;
+    private ArrayList<Dialog> mDialogs;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //Advisor lists are temp storage of type Advisor object
-        ArrayList<Advisor> starredAdvisorList = new ArrayList<>();
-        ArrayList<Advisor> normalAdvisorList = new ArrayList<>();
-        ArrayList<Advisor> starredContactsList = new ArrayList<>();
-        ArrayList<Advisor> normalContactsList = new ArrayList<>();
-
-        //Put all advisor names in an array, sort the array
-        ArrayList<String> advisorNames = new ArrayList<>();
-        for(int i=0;i<advisorList.size();i++) {
-            advisorNames.add(advisorList.get(i).advisorName);
-        }
-        Collections.sort(advisorNames);
-
-        //For each name, find the corresponding object through its index, then sort favourites out
-        for(int i=0;i<advisorNames.size();i++) {
-            int indexOfAdvisor = 0;
-            for (int j=0; j<advisorList.size();j++) {
-                if (advisorList.get(j).advisorName == advisorNames.get(i)) {
-                    //therefore: advisorName[i] corresponds to normalAdvisorList[j]
-                    indexOfAdvisor = j;
-                    break;
-                }
-            }
-            if(advisorList.get(indexOfAdvisor).isFavourite) {
-                starredAdvisorList.add(advisorList.get(indexOfAdvisor));
-            }else {
-                normalAdvisorList.add(advisorList.get(indexOfAdvisor));
-            }
-        }
-
-        //For all favourited advisors
-        for(int i=0;i<starredAdvisorList.size();i++) {
-            int image = starredAdvisorList.get(i).advisorImage;
-            String name = starredAdvisorList.get(i).advisorName;
-            String star = "★";
-            if(i > 0) {
-                star = "";
-            }
-            starredContactsList.add(new Advisor(name, "", star, "", image, false));
-        }
-
-        //For all normal advisors
-        ArrayList<String> usedLetters = new ArrayList<>();
-        for(int i=0;i<normalAdvisorList.size();i++) {
-            int image1 = normalAdvisorList.get(i).advisorImage;
-            String name1 = normalAdvisorList.get(i).advisorName;
-            String letter1 = "";
-            if(usedLetters.indexOf(Character.toString(normalAdvisorList.get(i).advisorName.charAt(0)).toUpperCase()) == -1) {
-                letter1 = Character.toString(normalAdvisorList.get(i).advisorName.charAt(0)).toUpperCase();
-                usedLetters.add(letter1);
-            }
-            normalContactsList.add(new Advisor(name1, "", letter1, "", image1, false));
-        }
-
-        //Put all the contact items in the same array list
-        ArrayList<Advisor> allContactItems = new ArrayList<>();
-        for(int i=0;i<starredContactsList.size();i++) {
-            allContactItems.add(starredContactsList.get(i));
-        }
-        for(int i=0;i<normalContactsList.size();i++) {
-            allContactItems.add(normalContactsList.get(i));
-        }
-
 
         View root = inflater.inflate(R.layout.fragment_contacts, container, false);
-        mRecyclerView = root.findViewById(R.id.contactsRecyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLinearLayoutManager = new LinearLayoutManager(thisActivity);
 
-        //Put the array contents into the recyclerview via the adapter
-        mAdapter = new ContactAdapter(getContext(), allContactItems);
+        mDialogsListView = root.findViewById(R.id.dialogsList);
+        mDialogs = new ArrayList<>();
+        createDialogs();
 
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        mAdapter = new DialogsListAdapter<>(null);
+
+        mDialogsListView.setAdapter(mAdapter);
+        mAdapter.setItems(mDialogs);
+
 
         return root;
     }
@@ -112,5 +54,14 @@ public class ContactsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+    private void createDialogs() {
+        Advisor advisor = Singleton.getInstance().advisors.get(0);
+        User user = Singleton.getInstance().user;
+        ArrayList<Author> users = new ArrayList<>();
+        users.add(advisor);
+        users.add(user);
+        mDialogs.add(new Dialog(advisor.id, advisor.avatar, advisor.name, users, Singleton.getInstance().chatHistory.get("0").get(Singleton.getInstance().chatHistory.get("0").size()-1), 0));
     }
 }
