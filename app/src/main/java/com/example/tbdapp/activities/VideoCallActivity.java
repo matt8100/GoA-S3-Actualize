83954d06b3340d255efd612fab1c5af275acb078
Markus Nimi <markusnimi@gmail.com>
2020-04-28 22:42:02 -0700

    Added animation indicator, toast with a message, and "recording" text when recording

--- app/src/main/java/com/example/tbdapp/activities/VideoCallActivity.java
+++ app/src/main/java/com/example/tbdapp/activities/VideoCallActivity.java
@@ -1,10 +1,16 @@
 package com.example.tbdapp.activities;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,6 +34,7 @@ public class VideoCallActivity extends AppCompatActivity {
         final ImageButton switchCamera = findViewById(R.id.fab_switch_camera);
         final ImageButton endCallButton = findViewById(R.id.fab_hangUp);
         final VideoView mainVideo = findViewById(R.id.mainVideo);
+        final TextView recordingIndicator = findViewById(R.id.recording_indicator);
         final String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.test_video1;
         final VideoView secondaryVideo = findViewById(R.id.secondaryVideo);
         final String videoPath2 = "android.resource://" + getPackageName() + "/" + R.raw.test_video2;
@@ -91,15 +98,46 @@ public class VideoCallActivity extends AppCompatActivity {
         });
 
         //Record
+        final ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                recordButton,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f));
+        scaleDown.setDuration(930);
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+
+        final ObjectAnimator resetAnimation = ObjectAnimator.ofPropertyValuesHolder(
+                recordButton,
+                PropertyValuesHolder.ofFloat("scaleX", 1f),
+                PropertyValuesHolder.ofFloat("scaleY", 1f));
+        resetAnimation.setDuration(100);
+
+        recordingIndicator.setVisibility(View.INVISIBLE);
         recordButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 recording = !recording;
+
                 if(recording) {
+                    recordingIndicator.setVisibility(View.VISIBLE);
                     recordButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop_recording, getTheme()));
                     recordingStart.start();
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Now Recording – Visible to Advisor";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    resetAnimation.cancel();
+                    scaleDown.start();
+
                 }else {
+                    recordingIndicator.setVisibility(View.INVISIBLE);
                     recordButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_record, getTheme()));
                     recordingEnd.start();
+
+                    scaleDown.cancel();
+                    resetAnimation.start();
                 }
             }
         });
