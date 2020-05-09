4c2e19b0336ce14e8d5fe7354dd1a4342abac759
Hod Kimhi <hodkimhi@gmail.com>
2020-05-08 19:33:23 -0700

    Added custom search bar to explore fragment.

--- app/src/main/java/com/example/tbdapp/fragments/ExploreFragment.java
+++ app/src/main/java/com/example/tbdapp/fragments/ExploreFragment.java
@@ -1,9 +1,14 @@
 package com.example.tbdapp.fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +27,7 @@ public class ExploreFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private View searchView;
 
     private ArrayList<Advisor> advisorList;
 
@@ -47,6 +53,44 @@ public class ExploreFragment extends Fragment {
         mAdapter = new ExploreAdapter(getContext(), advisorList);
         recyclerView.setAdapter(mAdapter);
 
+        setupSearchBar(rootView);
+
         return rootView;
+    }
+
+    private void setupSearchBar(View rootView) {
+        searchView = rootView.findViewById(R.id.searchView);
+        final EditText et_search = searchView.findViewById(R.id.edit_text_search);
+        final ImageButton ib_clear = searchView.findViewById(R.id.ib_clear_text);
+
+        ib_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et_search.setText("");
+            }
+        });
+
+        TextWatcher searchViewTextWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().trim().length()==0) {
+                    ib_clear.setVisibility(View.GONE);
+                } else {
+                    ib_clear.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        et_search.addTextChangedListener(searchViewTextWatcher);
     }
 }
